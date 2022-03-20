f028bee67cda36edbe9e9ddc222184db2dda5564
WuYanzu <1931295119@qq.com>
2022-03-20 17:41:02 +0800

    RabbitMQ生产者

---
+++ rabbitMq-producer/src/main/java/edu/jxut/Producer.java
@@ -0,0 +1,84 @@
+package edu.jxut;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    static final String QUEUE_NAME = "simple_queue";
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        //1建立连接
+        ConnectionFactory factory=new ConnectionFactory();
+        //2设置参数
+        factory.setHost("211.159.223.55");
+        factory.setPort(5672);
+        factory.setVirtualHost("/itcast");
+        factory.setUsername("heima");
+        factory.setPassword("heima");
+
+
+
+        //3创建连接
+        Connection connection = factory.newConnection();
+        //4创建channel
+        Channel channel = connection.createChannel();
+        //5创建队列
+        channel.queueDeclare("hello world",true,false,false,null);
+
+        channel.basicPublish("","hello world",null,"你好".getBytes());
+
+        channel.close();
+        connection.close();
+
+    }
+
+    public void getConnection() throws IOException, TimeoutException {
+        //创建连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //主机地址;默认为 localhost
+        connectionFactory.setHost("localhost");
+        //连接端口;默认为 5672
+        connectionFactory.setPort(5672);
+        //虚拟主机名称;默认为 /
+        connectionFactory.setVirtualHost("/itcast");
+        //连接用户名；默认为guest
+        connectionFactory.setUsername("heima");
+        //连接密码；默认为guest
+        connectionFactory.setPassword("heima");
+
+        //创建连接
+        Connection connection = connectionFactory.newConnection();
+
+        // 创建频道
+        Channel channel = connection.createChannel();
+
+        // 声明（创建）队列
+        /**
+         * 参数1：队列名称
+         * 参数2：是否定义持久化队列
+         * 参数3：是否独占本次连接
+         * 参数4：是否在不使用的时候自动删除队列
+         * 参数5：队列其它参数
+         */
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+
+        // 要发送的信息
+        String message = "你好；小兔子！";
+        /**
+         * 参数1：交换机名称，如果没有指定则使用默认Default Exchage
+         * 参数2：路由key,简单模式可以传递队列名称
+         * 参数3：消息其它属性
+         * 参数4：消息内容
+         */
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println("已发送消息：" + message);
+
+        // 关闭资源
+        channel.close();
+        connection.close();
+    }
+}
